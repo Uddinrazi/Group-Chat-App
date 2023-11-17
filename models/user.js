7dad1b1970e60db85aea14e2eea64183043d77f1
Uddinrazi <uddinrazi88@gmail.com>
2023-11-17 16:16:41 +0530

    sign-up frontend

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../util/db')
+
+const User = sequelize.define('user', {
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        
+    },
+    
+    phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+   
+})
+
+module.exports = User;
